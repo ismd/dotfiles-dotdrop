8f4a3f2c1194a469072fe195377dec91585be0ed
Vladimir Kosteley <github@ismd.dev>
2019-06-20 22:49:59 +0300

    i3, polybar

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -7,7 +7,14 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 for m in $(polybar --list-monitors | cut -d":" -f1); do
-    MONITOR=$m polybar my &
+    if [ $m == 'HDMI-A-0' ] 
+    then
+        echo $m
+        MONITOR=$m TRAY_POS=center polybar --reload my &
+    else
+        echo $m
+        MONITOR=$m TRAY_POS=none polybar --reload my &
+    fi
 done
 
 echo "Polybar launched..."
