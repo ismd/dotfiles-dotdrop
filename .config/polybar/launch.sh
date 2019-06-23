1f0c777de4adfd0882f9e32568011554ad6148eb
Vladimir Kosteley <github@ismd.dev>
2019-06-23 14:20:28 +0300

    polybar

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+PRIMARY_OUTPUT="HDMI-A-0"
+
 # Terminate already running bar instances
 killall -q polybar
 
@@ -7,14 +9,19 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 for m in $(polybar --list-monitors | cut -d":" -f1); do
-    if [ $m == 'HDMI-A-0' ]
-    then
-        echo $m
-        MONITOR=$m TRAY_POS=right polybar --reload my &
+    if [ $m == $PRIMARY_OUTPUT ]; then
+        TRAY_POS=right
     else
-        echo $m
-        MONITOR=$m TRAY_POS=none polybar --reload my &
+        TRAY_POS=none
     fi
+
+    MONITOR=$m \
+    BAR_HEIGHT=27 \
+    FONT0="Hack:fontformat=truetype:size=10;2" \
+    FONT1="Hack:fontformat=truetype:size=10;2" \
+    FONT2="FontAwesome:fontformat=truetype:size=10;2" \
+    TRAY_POS=$TRAY_POS \
+    polybar --reload my &
 done
 
 echo "Polybar launched..."
