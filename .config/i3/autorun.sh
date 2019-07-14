a80e2f728bac6c24640138d804c0bc9a8c8616fd
Vladimir Kosteley <github@ismd.dev>
2019-07-14 15:04:45 +0300

    i3

--- .config/i3/autorun.sh
+++ .config/i3/autorun.sh
@@ -12,6 +12,9 @@ xset -dpms
 xset s off
 setxkbmap -layout us,ru -option grp:toggle
 
+# run compton -cCGfF -o 0.38 -O 200 -I 200 -t 0 -l 0 -r 3 -D2 -m 0.88
+run compton -f --vsync drm
+run dunst
 run nextcloud
 run screenshotgun
 run telegram-desktop
