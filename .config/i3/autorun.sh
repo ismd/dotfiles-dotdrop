c78d915905237f576c3ed72e63cdce8a06a61b8c
Vladimir Kosteley <github@ismd.dev>
2020-01-25 19:55:40 +0300

    i3

--- .config/i3/autorun.sh
+++ .config/i3/autorun.sh
@@ -15,7 +15,6 @@ setxkbmap -layout us,ru -option grp:toggle
 # run compton -cCGfF -o 0.38 -O 200 -I 200 -t 0 -l 0 -r 3 -D2 -m 0.88
 run compton -f --vsync drm
 run dunst
-run nextcloud
 run screenshotgun
 run telegram-desktop
 run terminator -e tmux
