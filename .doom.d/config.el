a003087bc738916bf9b7e1851571b836746774e5
Vladimir Kosteley <github@ismd.dev>
2021-02-05 00:49:34 +0300

    doom.d

---
+++ .doom.d/config.el
@@ -0,0 +1,124 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; sync' after modifying this file!
+
+
+;; Some functionality uses this to identify you, e.g. GPG configuration, email
+;; clients, file templates and snippets.
+(setq user-full-name "Vladimir Kosteley"
+      user-mail-address "zzismd@gmail.com")
+
+;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
+;; are the three important ones:
+;;
+;; + `doom-font'
+;; + `doom-variable-pitch-font'
+;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;;   presentations or streaming.
+;;
+;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
+;; font string. You generally only need these two:
+;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
+;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
+
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. This is the default:
+(setq doom-theme 'doom-one)
+
+;; If you use `org' and don't want your org files in the default location below,
+;; change `org-directory'. It must be set before org loads!
+(setq org-directory "~/Yandex.Disk/Documents/org/")
+
+;; This determines the style of line numbers in effect. If set to `nil', line
+;; numbers are disabled. For relative line numbers, set this to `relative'.
+(setq display-line-numbers-type t)
+
+
+;; Here are some additional functions/macros that could help you configure Doom:
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package!' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', relative to
+;;   this file. Emacs searches the `load-path' when you load packages with
+;;   `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
+;; This will open documentation for it, including demos of how they are used.
+;;
+;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
+;; they are implemented.
+
+;;
+;; Functions
+;;
+(defun ismd/delete-word (arg)
+  "Delete characters until encountering the end of a word.
+With argument ARG, do this that many times."
+  (interactive "p")
+  (delete-region (point) (progn (forward-word arg) (point))))
+
+(defun ismd/backward-delete-word (arg)
+  "Delete characters backward until encountering the beginning of a word.
+With argument ARG, do this that many times."
+  (interactive "p")
+  (delete-region (point) (progn (backward-word arg) (point))))
+
+(defun ismd/kill-line ()
+  (interactive)
+  (cond ((ismd/current-line-empty-p) (delete-char 1))
+    (t (delete-region (point) (line-end-position)))))
+
+(defun ismd/current-line-empty-p ()
+  (save-excursion
+    (beginning-of-line)
+    (looking-at "[[:space:]]*$")))
+
+;;
+;; Init
+;;
+(set-default-coding-systems 'utf-8)
+(setq-default tab-width 4)
+(global-auto-revert-mode 1)
+(+global-word-wrap-mode)
+(doom/set-frame-opacity 95)
+(setq large-file-warning-threshold nil)
+(setq company-idle-delay 0)
+(setq company-tooltip-idle-delay 0)
+
+(setq mouse-wheel-scroll-amount '(1 ((shift) . 1))) ;; one line at a time
+(setq mouse-wheel-progressive-speed nil) ;; don't accelerate scrolling
+
+(global-set-key (kbd "C-k") 'ismd/kill-line)
+(global-set-key (kbd "M-d") 'ismd/delete-word)
+(global-set-key (kbd "M-<backspace>") 'ismd/backward-delete-word)
+(global-set-key (kbd "C-M-g") 'revert-buffer)
+(global-set-key (kbd "M-s s") 'avy-goto-char-timer)
+(global-set-key (kbd "M-n") "\C-u3\C-v")
+(global-set-key (kbd "M-p") "\C-u3\M-v")
+(global-set-key (kbd "C-s") '+default/search-buffer)
+(global-set-key (kbd "C-c <tab>") 'previous-buffer)
+
+(global-set-key (kbd "M-0") 'treemacs-select-window)
+(global-set-key (kbd "M-1") 'winum-select-window-1)
+(global-set-key (kbd "M-2") 'winum-select-window-2)
+(global-set-key (kbd "M-3") 'winum-select-window-3)
+(global-set-key (kbd "M-4") 'winum-select-window-4)
+(global-set-key (kbd "M-5") 'winum-select-window-5)
+(global-set-key (kbd "M-6") 'winum-select-window-6)
+(global-set-key (kbd "M-7") 'winum-select-window-7)
+(global-set-key (kbd "M-8") 'winum-select-window-8)
+(global-set-key (kbd "M-9") 'winum-select-window-9)
+
+(use-package! super-save
+  :ensure t
+  :config
+  (super-save-mode +1)
+  (setq super-save-auto-save-when-idle t))
+
+(after! ivy
+  (setq ivy-wrap nil))
