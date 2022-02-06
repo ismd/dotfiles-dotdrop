88de13ccc94029d722f0928809c4dc7a65111fef
Vladimir Kosteley <github@ismd.dev>
2022-02-06 15:21:52 +0300

    fish +PATH

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,4 +2,4 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-fish_add_path -p $HOME/bin $HOME/.dotfiles/bin $HOME/.emacs.d/bin
+fish_add_path -p $HOME/bin $HOME/.dotfiles/bin $HOME/.emacs.d/bin $HOME/Yandex.Disk/bin
