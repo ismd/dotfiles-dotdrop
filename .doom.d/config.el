297988f05f73647c56757c1b89c0e5192ef5c236
Vladimir Kosteley <github@ismd.dev>
2022-07-07 10:20:19 +0300

    doom

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -34,7 +34,7 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-vibrant)
+(setq doom-theme 'doom-one)
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
@@ -182,6 +182,23 @@ With argument ARG, do this that many times."
 
 ;; Vertico
 (setq vertico-cycle nil)
+
+;; Projectile
+(setq projectile-project-search-path '(("~/coding/" . 1) ("~/a/data-ui/" . 1)))
+
+(projectile-register-project-type 'npm '("package.json")
+  :project-file "package.json"
+  :compile "npm install"
+  :test "npm test"
+  :run "npm start"
+  :test-suffix ".spec")
+
+(projectile-register-project-type 'arcadia '("a.yaml")
+  :project-file "a.yaml"
+  :compile "npm install"
+  :test "npm test"
+  :run "npm start"
+  :test-suffix ".spec")
 
 ;; C/C++
 (after! c
