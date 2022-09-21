37d14fa6f0057283b7316ad7787030535719189d
Vladimir Kosteley <github@ismd.dev>
2022-09-21 13:42:28 +0300

    configs

--- bin/autorandr.sh
+++ bin/autorandr.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
 autorandr -c
+~/.dotfiles/bin/wal.sh
--- bin/wal.sh
+++ bin/wal.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-INPUT=${1:-$HOME/Pictures/Wallpapers}
+#INPUT=${1:-$HOME/Pictures/Wallpapers}
+INPUT=${1:-$HOME/Pictures/Wallpapers/john-fowler-onYwpkAI8ow-unsplash.jpg}
 
 wal -i $INPUT
 pywalfox update
