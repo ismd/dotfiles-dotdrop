ce4ca56d05a68eb310dbcadcbddbc9c178682b83
Vladimir Kosteley <github@ismd.dev>
2022-10-04 10:25:01 +0300

    ws-butler, editorconfig

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -35,7 +35,7 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'ewal-doom-one)
+(setq doom-theme 'doom-one-light)
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
@@ -212,8 +212,9 @@ With argument ARG, do this that many times."
 ;; ws-butler
 ;; Doesn't work right now
 ;; (remove-hook 'doom-first-buffer-hook #'ws-butler-global-mode)
-;; ;; (after! ws-butler
-;; ;;   (add-to-list 'ws-butler-global-exempt-modes 'org-mode))
+;; It works
+(after! ws-butler
+  (add-to-list 'ws-butler-global-exempt-modes 'org-mode))
 ;; (add-hook! org 'doom-disable-delete-trailing-whitespace-h)
 
 ;; Tabnine
@@ -221,7 +222,9 @@ With argument ARG, do this that many times."
   :ensure t
   :after company
   :config
-  (add-to-list 'company-backends #'company-tabnine))
+  (add-to-list 'company-backends #'company-tabnine)
+  ;; Trigger completion immediately.
+  (setq company-idle-delay 1.2))
 
 ;; OLD
 
