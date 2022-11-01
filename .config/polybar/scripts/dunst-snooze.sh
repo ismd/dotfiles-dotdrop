3f5e2b2e2691b4e3924017e11b0b6ff1b512e669
Vladimir Kosteley <github@ismd.dev>
2022-11-02 00:34:02 +0100

    polybar scripts

---
+++ .config/polybar/scripts/dunst-snooze.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+case "$1" in
+    --toggle)
+        dunstctl set-paused toggle
+        ;;
+    *)
+        if [ "$(dunstctl is-paused)" = "true" ]; then
+            echo "🔕"
+        else
+            echo ""
+        fi
+        ;;
+esac
