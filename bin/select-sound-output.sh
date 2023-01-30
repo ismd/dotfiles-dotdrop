6b7714b809ea1741205fb04cbedc04dade69e8f0
Vladimir Kosteley <github@ismd.dev>
2023-01-30 19:18:21 +0100

    Select sound output, i3 custom.conf

---
+++ bin/select-sound-output.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+OUTPUT=$(echo -e "HDMI\nRazer" | dmenu -fn "DaddyTimeMono Nerd Font-15" -p 'Select sound output:' -nb "#222D31" -nf "#F9FAF9" -sf "#F9FAF9" -sb "#16A085")
+
+case $OUTPUT in
+  HDMI) pactl set-default-sink alsa_output.pci-0000_2d_00.1.hdmi-stereo-extra1 ;;
+  Razer) pactl set-default-sink alsa_output.usb-Razer_Razer_USB_Sound_Card_00000000-00.analog-stereo ;;
+  *) echo "Specify output" ;;
+esac
