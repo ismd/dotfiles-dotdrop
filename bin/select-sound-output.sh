5da75a68479237843ce41ffaca669b2b0dacc740
Vladimir Kosteley <github@ismd.dev>
2023-06-04 09:51:44 +0200

    configs

--- bin/select-sound-output.sh
+++ bin/select-sound-output.sh
@@ -3,7 +3,7 @@
 OUTPUT=$(echo -e "HDMI\nRazer" | dmenu -l 3 -fn "DaddyTimeMono Nerd Font-15" -p 'Select sound output:' -nb "#222D31" -nf "#F9FAF9" -sf "#F9FAF9" -sb "#16A085")
 
 case $OUTPUT in
-  HDMI) pactl set-default-sink alsa_output.pci-0000_2d_00.1.hdmi-stereo-extra1 ;;
+  HDMI) pactl set-default-sink alsa_output.pci-0000_2d_00.1.hdmi-stereo ;;
   Razer) pactl set-default-sink alsa_output.usb-Razer_Razer_USB_Sound_Card_00000000-00.analog-stereo ;;
   *) echo "Specify output" ;;
 esac
