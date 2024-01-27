ccdde67490faa60189ca9075e023ad40a0384a66
Vladimir Kosteley <github@ismd.dev>
2024-01-27 15:51:37 +0100

    Emacs config

---
+++ conf/config/emacs/init.el
@@ -0,0 +1,12 @@
+;;; init.el --- Emacs configuration -*- lexical-binding: t; -*-
+
+;;; Commentary:
+;;; Code:
+
+(org-babel-load-file
+ (expand-file-name
+  "config.org"
+  user-emacs-directory))
+(put 'dired-find-alternate-file 'disabled nil)
+
+;;; init.el ends here
