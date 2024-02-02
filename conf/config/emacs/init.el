debeebfc20c2c5b1b84eee38923963063261e2f3
Vladimir Kosteley <github@ismd.dev>
2024-02-02 19:53:32 +0100

    Ibuffer

--- conf/config/emacs/init.el
+++ conf/config/emacs/init.el
@@ -4,9 +4,9 @@
 ;;; Code:
 
 (org-babel-load-file
- (expand-file-name
-  "config.org"
-  user-emacs-directory))
+  (expand-file-name
+    "config.org"
+    user-emacs-directory))
 (put 'dired-find-alternate-file 'disabled nil)
 
 ;;; init.el ends here
