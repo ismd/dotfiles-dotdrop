7b0278d7227ad163941f03b6f1079283733a0764
Vladimir Kosteley <github@ismd.dev>
2024-03-18 18:03:47 +0100

    profile

--- conf/config/fish/config.fish
+++ conf/config/fish/config.fish
@@ -4,23 +4,16 @@ end
 
 # PATH
 fish_add_path -p ~/.bin
-fish_add_path -p ~/.local/share/nvm/v20.9.0/bin
+fish_add_path -p ~/.local/share/nvm/v20.11.1/bin
 fish_add_path -p ~/.config/emacs/bin
 fish_add_path -p ~/src/flutter/bin
 
-# nvim
-alias nv=nvim
-
-# dotdrop
+# Aliases
+alias alacritty="alacritty --config-file ~/.config/i3/alacritty/alacritty.toml"
 alias dotdrop='dotdrop --cfg=~/.dotfiles/config.yaml'
-
-# ripgrep
-export RIPGREP_CONFIG_PATH=$HOME/.ripgreprc
+alias nv=nvim
 
 {%@@ if profile == "cz-work" @@%}
 # Skotty
 eval $(skotty ssh env)
 {%@@ endif @@%}
-
-# alacritty
-alias alacritty="alacritty --config-file ~/.config/i3/alacritty/alacritty.toml"
