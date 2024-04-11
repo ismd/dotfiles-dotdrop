c68df78c3c79dbd00a98357825582d74519860b5
Vladimir Kosteley <github@ismd.dev>
2024-04-11 12:52:16 +0200

    New configs

--- conf/config/fish/config.fish
+++ conf/config/fish/config.fish
@@ -6,7 +6,7 @@ set fish_greeting
 
 # PATH
 fish_add_path -p ~/.bin
-fish_add_path -p ~/.local/share/nvm/v20.11.1/bin
+fish_add_path -p ~/.local/share/nvm/v20.12.1/bin
 fish_add_path -p ~/.config/emacs/bin
 fish_add_path -p ~/src/flutter/bin
 
@@ -14,8 +14,3 @@ fish_add_path -p ~/src/flutter/bin
 alias alacritty="alacritty --config-file ~/.config/i3/alacritty/alacritty.toml"
 alias dotdrop='dotdrop --cfg=~/.dotfiles/config.yaml'
 alias nv=nvim
-
-{%@@ if profile == "cz-work" @@%}
-# Skotty
-eval $(skotty ssh env)
-{%@@ endif @@%}
