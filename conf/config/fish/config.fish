a306a0f03cd279a7fdef936632f5d74d66353f4b
Vladimir Kosteley <github@ismd.dev>
2025-02-01 10:13:49 +0100

    feat(fish): update path

--- conf/config/fish/config.fish
+++ conf/config/fish/config.fish
@@ -6,7 +6,8 @@ set fish_greeting
 
 # PATH
 fish_add_path -p ~/.bin
-fish_add_path -p ~/.local/share/nvm/v20.17.0/bin
+fish_add_path -p ~/.local/bin
+fish_add_path -p ~/.local/share/nvm/v20.18.1/bin
 fish_add_path -p ~/.config/emacs/bin
 fish_add_path -p ~/src/flutter/bin
 fish_add_path -p ~/.pub-cache/bin
