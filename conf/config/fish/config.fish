0600a7623b3a5f7f68450a54a0361b2d0e413dc5
Vladimir Kosteley <github@ismd.dev>
2025-02-17 16:11:37 +0100

    feat(fish): update nvm version

--- conf/config/fish/config.fish
+++ conf/config/fish/config.fish
@@ -7,7 +7,7 @@ set fish_greeting
 # PATH
 fish_add_path -p ~/.bin
 fish_add_path -p ~/.local/bin
-fish_add_path -p ~/.local/share/nvm/v20.18.1/bin
+fish_add_path -p ~/.local/share/nvm/v20.18.2/bin
 fish_add_path -p ~/.config/emacs/bin
 fish_add_path -p ~/src/flutter/bin
 fish_add_path -p ~/.pub-cache/bin
