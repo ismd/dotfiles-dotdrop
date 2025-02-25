92d8c677c3121663119a539eaa80d05052cdfd73
Vladimir Kosteley <github@ismd.dev>
2025-02-25 13:32:40 +0100

    feat(fish): add abbrs

--- conf/config/fish/config.fish
+++ conf/config/fish/config.fish
@@ -13,14 +13,19 @@ fish_add_path -p ~/.pub-cache/bin
 abbr -a aci arc commit
 abbr -a aco arc checkout
 abbr -a acob arc checkout -b
-abbr -a dc dotdrop compare
-abbr -a di dotdrop install
+abbr -a adi arc diff
+abbr -a apl arc pull
+abbr -a aps arc push
+abbr -a ast arc status
+abbr -a dco dotdrop compare
+abbr -a din dotdrop install
 abbr -a gci git commit
 abbr -a gco git checkout
 abbr -a gcob git checkout -b
-abbr -a gd git diff
-abbr -a gp git pull
-abbr -a gs git status
+abbr -a gdi git diff
+abbr -a gpl git pull
+abbr -a gps git push
+abbr -a gst git status
 
 # Aliases
 alias dotdrop '~/.dotfiles/dotdrop.sh --cfg=~/.dotfiles/config.yaml'
